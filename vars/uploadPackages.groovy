f7dd094378358faeab5eeabd6aa387f08b2c3856
Mattia Dal Ben <matthewdibi@gmail.com>
2025-09-09 15:59:54 +0200

    fix: remove unnecessary stuff

--- vars/uploadPackages.groovy
+++ vars/uploadPackages.groovy
@@ -1,11 +1,5 @@
 def call(String repoDistribution, String repoModule, Boolean setupPromotion = false) {
-    def DEV_REPO = "kura-develop-deb"
-    def PROD_REPO = "kura-deb"
-    def DEB_COMPONENT = "main"
-    def VALID_DEB_ARCHITECTURES = ["amd64", "arm64", "all"]
-
     stage ("Upload packages parameters check") {
-
         echo "Distribution: ${repoDistribution}"
         echo "Module: ${repoModule}"
 
@@ -26,21 +20,7 @@ def call(String repoDistribution, String repoModule, Boolean setupPromotion = fa
         def debFilesOutput = sh(script: "find workdir -type f -name '*.deb'", returnStdout: true).trim()
         def debFiles = debFilesOutput ? debFilesOutput.split("\n") : []
 
-        // Debug print all found .deb files
-        echo "Found .deb files:"
-        debFiles.each { echo it.toString() }
-        assert debFiles.size() > 0
-        echo "Total .deb files found: ${debFiles.size()}"
-
         debFiles.each {
-            // Split file name to get the architecture
-            def fileName = it.toString().split("/").last()
-            def architecture = fileName.split("_").last().split("\\.")[0]
-            assert VALID_DEB_ARCHITECTURES.contains(architecture)
-
-            // Print filename and architecture
-            echo "Uploading file: ${fileName} with architecture: ${architecture}"
-
             withCredentials([usernameColonPassword(credentialsId: 'repo.eclipse.org-bot-account', variable: 'USERPASS')]) {
                 sh(
                     script: "curl -u \"\$USERPASS\" -H \"Content-Type: multipart/form-data\" --data-binary \"@./${it}\" \"https://repo3.eclipse.org/repository/kura-apt/\"",
